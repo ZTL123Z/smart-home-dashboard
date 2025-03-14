e88d7cfe88cc3aeab450277340168078225479d0
ZTL123Z <2746395413@qq.com>
2025-03-14 11:10:00 +0800

    去掉不合适标题

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MainContent from './components/MainContent';
 function App() {
   return (
     <div className="app">
-      <div className="app-title">Domotica</div>
       <div className="app-container">
         <Sidebar />
         <MainContent />
